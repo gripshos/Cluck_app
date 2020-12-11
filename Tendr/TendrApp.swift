e83070f16f50e36e6c90fa0e95ee284dd7a239be
Steven Gripshover <gripshos@fuse.net>
2020-12-11 10:46:34 -0500

    Initial Commit

---
+++ Tendr/TendrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TendrApp.swift
+//  Tendr
+//
+//  Created by Steven Gripshover on 12/11/20.
+//
+
+import SwiftUI
+
+@main
+struct TendrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
